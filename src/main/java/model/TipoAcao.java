5da8fe62641b054bc670e8e4908f204cdfea6329
Lucas Dahbar <lucasgdahbar@gmail.com>
2026-01-23 21:17:26 -0300

    Transforma TipoAcao em enum

--- src/main/java/model/TipoAcao.java
+++ src/main/java/model/TipoAcao.java
@@ -1,10 +1,7 @@
 package model;
 
-public class TipoAcao {
-    public static final String ORDINARIA = "ORDINARIA";
-    public static final String PREFERENCIAL = "PREFERENCIAL";
-    public static final String UNIT = "UNIT";
-
-    private TipoAcao() {
-    }
+public enum TipoAcao {
+    ORDINARIA,
+    PREFERENCIAL,
+    UNIT
 }
