885f72f69660acdfda50ce74635441913a749f76
Lucas Dahbar <lucasgdahbar@gmail.com>
2026-01-23 21:16:47 -0300

    Transforma TipoRenda em enum

--- src/main/java/model/TipoRenda.java
+++ src/main/java/model/TipoRenda.java
@@ -1,11 +1,6 @@
 package model;
 
-public class TipoRenda {
-
-    public static final String FIXA = "FIXA";
-    public static final String VARIAVEL = "VARIAVEL";
-
-    // construtor privado para impedir instanciação
-    private TipoRenda() {
-    }
+public enum TipoRenda {
+    FIXA,
+    VARIAVEL
 }
