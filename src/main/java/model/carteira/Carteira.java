e5923313aae7caccaa075c0c517c5fc9cd96f20c
Lucas Dahbar <lucasgdahbar@gmail.com>
2026-01-26 13:41:53 -0300

    Corrige erro de sintaxe do Map em Carteira

--- src/main/java/model/carteira/Carteira.java
+++ src/main/java/model/carteira/Carteira.java
@@ -14,7 +14,7 @@ import java.util.Map;
 public class Carteira {
 
     private final Map<Ativo, BigDecimal> ativos = new HashMap<>();
-    private final Map<Ativo, java.math.BigDecimal> valorGastoPorAtivo = new java.util.HashMap<>();
+    private final Map<Ativo, BigDecimal> custoTotalPosicaoEmReal = new HashMap<>();
 
 
     // Adiciona ativos (compra)
